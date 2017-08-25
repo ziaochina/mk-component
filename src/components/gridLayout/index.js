3727746926e50e2a69c2a0202a76bbf47fc6d773
ziaochina <ziaochina@gmail.com>
2017-08-25 11:30:40 +0800

    zlj

--- src/components/gridLayout/index.js
+++ src/components/gridLayout/index.js
@@ -1,3 +1,9 @@
 import GridLayout from 'react-grid-layout'
 
+const { WidthProvider, Responsive } = GridLayout
+
+GridLayout.WidthProviderGridLayout = WidthProvider(GridLayout)
+
+GridLayout.WidthProviderResponsive = WidthProvider(Responsive)
+
 export default GridLayout
