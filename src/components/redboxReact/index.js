d548b97367f406c491db9070c6a64f4904b78ab5
ziaochina <ziaochina@gmail.com>
2017-09-04 15:10:52 +0800

    zlj

--- src/components/redboxReact/index.js
+++ src/components/redboxReact/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import RedBox from 'redbox-react'
 import classNames from 'classnames'
 
-function RedboxReactComponent(props) {
+export default function RedboxReactComponent(props) {
 	let className = classNames({
 		'mk-redbox-react': true,
 		[props.className]: !!props.className
