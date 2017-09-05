69239574810302313ce8cd471af6f459dc994efe
ziaochina <ziaochina@gmail.com>
2017-09-05 17:30:23 +0800

    zlj

--- src/components/redboxReact/index.js
+++ src/components/redboxReact/index.js
@@ -1,11 +1,68 @@
 import React from 'react'
-import RedBox from 'redbox-react'
+import RedBox, { RedBoxError } from 'redbox-react'
 import classNames from 'classnames'
 
-export default function RedboxReactComponent(props) {
+const STYLE = {
+	redbox: {
+		boxSizing: 'border-box',
+		fontFamily: 'sans-serif',
+		position: 'relative',
+		padding: 10,
+		top: '0px',
+		left: '0px',
+		bottom: '0px',
+		right: '0px',
+		width: '100%',
+		background: 'rgb(204, 0, 0)',
+		color: 'white',
+		zIndex: 2147483647,
+		textAlign: 'left',
+		fontSize: '16px',
+		lineHeight: 1.2,
+		overflow: 'auto'
+	},
+	message: {
+		fontWeight: 'bold'
+	},
+	stack: {
+		fontFamily: 'monospace',
+		marginTop: '2em'
+	},
+	frame: {
+		marginTop: '1em'
+	},
+	file: {
+		fontSize: '0.8em',
+		color: 'rgba(255, 255, 255, 0.7)'
+	},
+	linkToFile: {
+		textDecoration: 'none',
+		color: 'rgba(255, 255, 255, 0.7)'
+	}
+}
+
+function RedboxReactComponent(props) {
 	let className = classNames({
 		'mk-redbox-react': true,
 		[props.className]: !!props.className
 	})
 	return <RedBox {...props} className={className} />
 }
+
+function RedBoxErrorComponent(props) {
+	let className = classNames({
+		'mk-redbox-error': true,
+		[props.className]: !!props.className
+	})
+
+	var style = STYLE
+	if (props.style) {
+		style = { ...style, ...props.style }
+	}
+
+	return <RedBoxError {...props} className={className} style={style} />
+}
+
+RedboxReactComponent.RedBoxError = RedBoxErrorComponent
+
+export default RedboxReactComponent
