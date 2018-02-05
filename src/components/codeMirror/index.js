87c38de7e3ee8d24703d023a3c92b97cd0871270
ziaochina <ziaochina@gmail.com>
2018-02-05 15:17:09 +0800

    zlj

--- src/components/codeMirror/index.js
+++ src/components/codeMirror/index.js
@@ -4,7 +4,7 @@ import 'codemirror/theme/material.css'
 import "codemirror/lib/codemirror.css"
 import "codemirror/mode/javascript/javascript"
 import "codemirror/mode/css/css"
-import "codemirror/mode/javascript/markdown"
+import "codemirror/mode/markdown/markdown"
 
 export default function CodeMirrorComponent(props) {
 	return <CodeMirror {...props}/>
