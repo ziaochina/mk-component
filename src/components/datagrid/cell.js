493a88ae63c6caab33fd70c3da1d11e697aaf6bc
ziaochia <ziaochina@gmail.com>
2018-05-31 12:51:23 +0800

    zlj

--- src/components/datagrid/cell.js
+++ src/components/datagrid/cell.js
@@ -1,8 +1,39 @@
 import React, { Component } from 'react'
 import { Cell } from 'fixed-data-table-2'
 import classNames from 'classnames'
+/*
+export default function cellComponent(props) {
+	const {
+		height,
+		width,
+		style,
+		className,
+		align,
+		children,
+		...other
+	} = props
 
-export default class cellComponent extends Component {
+	let cls = classNames({
+		'mk-datagrid-cellContent': true,
+		[`mk-datagrid-cellContent-${align}`]: !!align,
+		[className]: !!className
+	})
+
+	var innerStyle = {
+		height,
+		width,
+		...style,
+	}
+
+	return (
+		<div {...other} className={cls} style={innerStyle}>
+			{children}
+		</div>
+	)
+}*/
+
+
+export default class cellComponent extends React.PureComponent {
 	render() {
 		const {
       		height,
--- src/components/datagrid/textCell.js
+++ src/components/datagrid/textCell.js
@@ -1,9 +1,77 @@
 import React, { Component } from 'react'
 import { Cell } from 'fixed-data-table-2'
 import classNames from 'classnames'
+/*
+export default function textCellComponent(props) {
+    let {
+        height,
+          width,
+          style,
+          className,
+          align,
+          sand,
+          value,
+          dataType,
+          precision,
+          enableTooltip,
+          enableEllipsis,
+          ...other
+    } = props
+
+      let cls = classNames({
+          'mk-datagrid-cellContent': true,
+          [`mk-datagrid-cellContent-${align}`]: !!align,
+          [className]: !!className
+      })
+
+      let innerStyle = {
+          height,
+          width,
+          ...style,
+      }
+
+      switch (dataType) {
+          case 'bool':
+              value = (value == undefined || value == false) ? '否' : '是'
+              break
+          case 'float':
+              if (precision && (!!value)) {
+                  value = parseFloat(value).toFixed(precision)
+              }
+              break
+      }
+
+      if (value == null || value == undefined) {
+          value = ''
+      }
+
+      value = value + ''
+
+      if (sand) {
+          value = sand(value)
+      }
+
+      let ext = {}
+      if (enableTooltip) {
+          ext.title = value
+      }
 
 
-export default class textCellComponent extends Component {
+      if (enableEllipsis) {
+          ext.title = value
+          value = [<span style={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}>{value}</span>]
+      }
+
+
+
+      return (
+          <div {...other} className={cls} style={innerStyle} {...ext}>
+              {value}
+          </div>
+      )
+}*/
+
+export default class textCellComponent extends React.PureComponent {
     render() {
         let {
           height,
